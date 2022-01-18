ca96eb7c367632f4c3aee799351323791b462ba5
Hector <bahamondeh@gmail.com>
2022-01-18 17:43:07 +0200

    renames dir path

--- Voting_Phys_App.R
+++ Voting_Phys_App.R
@@ -3,7 +3,7 @@
 ##############################
 cat("\014")
 rm(list=ls())
-setwd("/Users/hectorbahamonde/research/Voting_Physical_Appearance/")
+setwd("/Users/hectorbahamonde/research/Physical/")
 
 # Pacman
 if (!require("pacman")) install.packages("pacman"); library(pacman) 
@@ -11,19 +11,19 @@ if (!require("pacman")) install.packages("pacman"); library(pacman)
 # importing data
 p_load(foreign,readxl,data.table)
 #
-phys.cong.d <- read.dta("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/phys_cong.dta") 
+phys.cong.d <- read.dta("/Users/hectorbahamonde/research/Physical/data/phys_cong.dta") 
 #
-id.1.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 1)
-id.2.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 2)
-id.3.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 3)
-id.4.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 4)
-id.5.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 5)
-id.6.d <- read_excel("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/id.xls", sheet = 6)
+id.1.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 1)
+id.2.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 2)
+id.3.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 3)
+id.4.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 4)
+id.5.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 5)
+id.6.d <- read_excel("/Users/hectorbahamonde/research/Physical/data/id.xls", sheet = 6)
 id.d = data.frame(rbind(id.1.d, id.2.d, id.3.d, id.4.d, id.5.d, id.6.d))
 #
 p_load(rio,tibble)
 electoral.d <- rio::import("https://seafile.utu.fi/f/295e920af4084e0c8102/?dl=1")
-# electoral.d = fread("/Users/hectorbahamonde/research/Voting_Physical_Appearance/data/electoral_data.csv")
+# electoral.d = fread("/Users/hectorbahamonde/research/Physical/data/electoral_data.csv")
 p_load(dplyr)
 electoral.d = electoral.d %>% 
   group_by(municipality,party,city,firstname,lastname,occup.elect.off) %>% 
